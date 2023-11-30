2191df6b0841f120a2af536cbb73629a903c6621
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-01 01:31:11 +0900

    feature(Test): Add UserRepositoryTest
    1. Add UserRepository Test about findByUsername success
    
    2. Add UserRepository Test about findByUsername fail
    
    3. Add given-when-then keyword

--- src/test/java/com/sparta/todoapp/repository/UserRepositoryTest.java
+++ src/test/java/com/sparta/todoapp/repository/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.sparta.todoapp.repository;
 
 import com.sparta.todoapp.entity.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,25 +9,76 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@ActiveProfiles("test")
-class UserRepositoryTest {
+@ActiveProfiles("test")     // application-test.properties에 저장되어있는 db와 연동
+class UserRepositoryTest extends RepositoryTest{
     @Autowired
     private UserRepository userRepository;
 
     @Test
+    @DisplayName("입력한 username과 password에 해당하는 user Entity db에 저장시키기")
     public void saveTest(){
+        // given
         User user = User.builder()
                 .username("test")
                 .password("test1234")
                 .build();
 
+        // when
         User saveUser = userRepository.save(user);
 
+        // then
         assertThat(user.getUsername()).isEqualTo(saveUser.getUsername());
         assertThat(user.getPassword()).isEqualTo(saveUser.getPassword());
+    }
+
+    @Test
+    @DisplayName("찾고있는 username으로 user 데이터 찾아오기 성공")
+    public void findByUsernameTest() throws IllegalAccessException {
+        // given
+        User user = User.builder()
+                .username("test")
+                .password("test1234")
+                .build();
+        User saveUser = userRepository.save(user);
+
+        // when
+        User findUser = userRepository.findByUsername(user.getUsername()).orElseThrow(
+                () -> new IllegalAccessException("해당하는 사용자가 존재하지않습니다.")
+        );
+
+        // then
+        assertThat(saveUser.getUsername()).isEqualTo(findUser.getUsername());
+        assertThat(saveUser.getPassword()).isEqualTo(findUser.getPassword());
+    }
+
+    @Test
+    @DisplayName("찾고있는 username으로 user 데이터 찾아오기 실패")
+    public void findByUsernameFailTest() throws IllegalAccessException {
+        // given
+        User user = User.builder()
+                .username("test")
+                .password("test1234")
+                .build();
+
+        User wrongUser = new User();
+        wrongUser.setUsername("Wrong");
+
+        User saveUser = userRepository.save(user);
+
+        // when
+        Exception exception = assertThrows(NullPointerException.class,
+                () -> userRepository.findByUsername(wrongUser.getUsername()).orElseThrow(
+                        () -> new NullPointerException("해당 유저는 존재하지 않습니다.")
+                )
+        );
+
+        // then
+        assertEquals("해당 유저는 존재하지 않습니다.", exception.getMessage());
     }
 }
