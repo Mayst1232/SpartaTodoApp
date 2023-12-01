74be0e4db61998f5d4cdc2037f90b8da72d7430b
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-01 16:34:36 +0900

    comment : Add given-when-then keyword
    feature : Add getTitleCardsTest in CardServiceTest & several exception Test

--- src/test/java/com/sparta/todoapp/service/CardServiceTest.java
+++ src/test/java/com/sparta/todoapp/service/CardServiceTest.java
@@ -97,6 +97,7 @@ class CardServiceTest {
     @Test
     @DisplayName("할일 카드의 전체 조회 중 visible이 true인 카드들만 조회하는 기능 테스트")
     void getAllCardsTest() {
+        // given
         List<Card> cardList = new ArrayList<>();
         User user = new User("Hwang", "1234");
 
@@ -110,14 +111,89 @@ class CardServiceTest {
 
         given(cardRepository.findAllByVisible(true)).willReturn(cardList);
 
+        // when
         List<CardExceptCommentResponseDto> result = cardService.getAllCards();
 
+        // then
         assertThat(result).hasSize(2);
     }
 
     @Test
-    void getTitleCards() {
+    @DisplayName("찾고 있는 제목의 카드가 모두 visible true일 때 찾고 있는 모든 카드를 찾아주는 기능 테스트")
+    void getTitleCardsTest() {
+        // given
+        User user = new User();
+        List<Card> cardList = new ArrayList<>();
 
+        for(int i = 0; i < 4; i++){
+            CardRequestDto requestDto = new CardRequestDto("할일 카드 제목 ", "할일 카드 내용 " +i, true, false);
+            Card card = new Card(requestDto, user);
+            cardList.add(card);
+        }
+
+        CardTitleRequestDto titleRequestDto = new CardTitleRequestDto("할일 카드 제목");
+
+        given(cardRepository.findAllByTitle(titleRequestDto.getTitle())).willReturn(cardList);
+
+        // when
+        List<CardExceptCommentResponseDto> result = cardService.getTitleCards(titleRequestDto, user);
+
+        // then
+        assertThat(result).hasSize(4);
+    }
+
+    @Test
+    @DisplayName("찾고 있는 제목의 카드가 존재하지 않아 예외를 발생시키는 기능 테스트")
+    void getTitleCardsFailCardListIsEmptyTest() {
+        // given
+        User user = new User();
+        List<Card> cardList = new ArrayList<>();
+
+        for(int i = 0; i < 4; i++){
+            CardRequestDto requestDto = new CardRequestDto("할일 카드 제목 ", "할일 카드 내용 " +i, true, false);
+            Card card = new Card(requestDto, user);
+            cardList.add(card);
+        }
+
+        CardTitleRequestDto titleRequestDto = new CardTitleRequestDto("없는 카드 제목");
+
+        given(cardRepository.findAllByTitle(titleRequestDto.getTitle()))
+                .willReturn(cardList = new ArrayList<>());
+
+        //when
+        Exception exception = assertThrows(NullPointerException.class, () -> cardService.getTitleCards(titleRequestDto, user));
+
+        assertThat("해당하는 제목의 게시물이 존재하지 않습니다.").isEqualTo(exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("찾고 있는 제목의 할일 카드가 visible이 false이지만 작성자가 본인이면 찾을 수 있게 해주는 기능 테스트")
+    void getTitleCardsVisibleIsFalseTest() {
+        // given
+        User user = new User();
+        List<Card> cardList = new ArrayList<>();
+
+        for(int i = 0; i < 4; i++){
+            CardRequestDto requestDto = new CardRequestDto("할일 카드 제목 ", "할일 카드 내용 " +i, false, false);
+            Card card = new Card(requestDto, user);
+            cardList.add(card);
+        }
+
+        CardTitleRequestDto titleRequestDto = new CardTitleRequestDto("없는 카드 제목");
+
+        given(cardRepository.findAllByTitle(titleRequestDto.getTitle()))
+                .willReturn(cardList);
+
+        for(Card card : cardList){
+            given(cardRepository.findByUserAndTitle(user, titleRequestDto.getTitle()))
+                    .willReturn(card);
+        }
+
+        //when
+        List<CardExceptCommentResponseDto> result = cardService.getTitleCards(titleRequestDto, user);
+
+        // then
+        assertThat(result).hasSize(4);
     }
 
     @Test
