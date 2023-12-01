c62c2d5c3586a056283ce304f9d11ab5cfaaca04
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-01 20:23:17 +0900

    feat: init controller Test

---
+++ src/test/java/com/sparta/todoapp/mvc/CommentControllerTest.java
@@ -0,0 +1,22 @@
+package com.sparta.todoapp.mvc;
+
+import org.junit.jupiter.api.Test;
+
+class CommentControllerTest {
+
+    @Test
+    void createComments() {
+    }
+
+    @Test
+    void getComments() {
+    }
+
+    @Test
+    void modifyComment() {
+    }
+
+    @Test
+    void deleteComment() {
+    }
+}
---
+++ src/test/java/com/sparta/todoapp/mvc/UserControllerTest.java
@@ -0,0 +1,10 @@
+package com.sparta.todoapp.mvc;
+
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+    @Test
+    void signup() {
+    }
+}
