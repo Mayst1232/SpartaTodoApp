c31f65a94ce7417a6d08e35d837d97970fbc93a2
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-01 10:39:13 +0900

    Comment : add comment in RepositoryTest

--- src/test/java/com/sparta/todoapp/repository/RepositoryTest.java
+++ src/test/java/com/sparta/todoapp/repository/RepositoryTest.java
@@ -4,6 +4,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
-@ActiveProfiles("test")
+@ActiveProfiles("test") // application-test.properties에 저장되어있는 db와 연동
 public class RepositoryTest {
 }
