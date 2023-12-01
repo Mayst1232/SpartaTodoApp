e202ecf85d091d8e007b6ea74087f1ee6f913ace
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-01 16:16:55 +0900

    feature(Test): Add getAllCardsTest in CardServiceTest
    refactory : not usage import & Add @Display & rename test Method name

--- src/test/java/com/sparta/todoapp/service/CardServiceTest.java
+++ src/test/java/com/sparta/todoapp/service/CardServiceTest.java
@@ -3,26 +3,25 @@ package com.sparta.todoapp.service;
 import com.sparta.todoapp.dto.CardExceptCommentResponseDto;
 import com.sparta.todoapp.dto.CardRequestDto;
 import com.sparta.todoapp.dto.CardResponseDto;
+import com.sparta.todoapp.dto.CardTitleRequestDto;
 import com.sparta.todoapp.entity.Card;
 import com.sparta.todoapp.entity.User;
 import com.sparta.todoapp.repository.CardRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.lenient;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -36,7 +35,8 @@ class CardServiceTest {
 
 
     @Test
-    void testCreateCard() {
+    @DisplayName("CardService의 createCard의 메소드 동작 테스트")
+    void createCardTest() {
         // given
         User user = new User();
         CardRequestDto requestDto = new CardRequestDto("할일 제목", "할일 내용", true, false);
@@ -53,32 +53,29 @@ class CardServiceTest {
     }
 
     @Test
-    void getCards() {
+    @DisplayName("할일 카드의 단건 조회하는 기능 중 작성자가 visible을 false로 설정하여도 자기 자신은 할일 카드를 확인할 수 있습니다.")
+    void getCardsSuccessTest() {
         // given
-        // 주어진 입력들
         CardRequestDto requestDto = new CardRequestDto("찾은 카드 제목", "찾은 카드 내용",false, false);
 
         User findUser = new User();
         findUser.setId(100L);
-
-        //Card card = Mockito.spy(new Card(requestDto, findUser));
-
         Card card = new Card(requestDto, findUser);
         card.setId(1L);
 
-        // 카드의 아이디는 1L 이다.
-//        Mockito.when(card.getId()).thenReturn(1L);
-        // 1L 인 카드를 조회하면 등록된 카드를 반환한다.
         given(cardRepository.findById(1L)).willReturn(Optional.of(card));
         given(cardRepository.findByUserAndId(findUser, 1L)).willReturn(Optional.of(card));
+
         // when
         CardResponseDto responseDto = cardService.getCards(1L, findUser);
+
         // then
         assertThat(responseDto.getContent()).isEqualTo(requestDto.getContent());
     }
 
     @Test
-    void getCardsfail(){  // 내가 작성한 카드가 아닌데 << visible -> false 그러면 안보여야되거든요?
+    @DisplayName("할일 카드의 단건 조회 중 작성자가 visible을 false로 설정해놓으면 다른 사람은 그 카드를 확인할 수 없습니다.")
+    void getCardsFailTest(){
         // given
         CardRequestDto requestDto = new CardRequestDto("찾은 카드 제목", "찾은 카드 내용", false, false);
         User createUser = new User();
@@ -90,19 +87,37 @@ class CardServiceTest {
         findUser.setId(200L);
 
         given(cardRepository.findById(1L)).willReturn(Optional.of(card));
-        given(cardRepository.findByUserAndId(createUser, 1L)).willReturn(Optional.of(card));
-        // when then
-//        System.out.println(cardRepository.findByUserAndId(findUser, 1L).isEmpty());
+        given(cardRepository.findByUserAndId(findUser, 1L)).willReturn(Optional.empty());
+
+        // when - then
         Exception exception = assertThrows(NullPointerException.class, () -> cardService.getCards(1L, findUser));
         assertThat("해당하는 카드가 없습니다.").isEqualTo(exception.getMessage());
     }
 
     @Test
-    void getAllCards() {
+    @DisplayName("할일 카드의 전체 조회 중 visible이 true인 카드들만 조회하는 기능 테스트")
+    void getAllCardsTest() {
+        List<Card> cardList = new ArrayList<>();
+        User user = new User("Hwang", "1234");
+
+        for(int i = 0; i < 4; i++){
+            CardRequestDto requestDto = new CardRequestDto("할일 카드 제목 " + i, "할일 카드 내용 " +i, i % 2 == 0, false);
+            Card card = new Card(requestDto, user);
+            if(card.isVisible()){
+                cardList.add(card);
+            }
+        }
+
+        given(cardRepository.findAllByVisible(true)).willReturn(cardList);
+
+        List<CardExceptCommentResponseDto> result = cardService.getAllCards();
+
+        assertThat(result).hasSize(2);
     }
 
     @Test
     void getTitleCards() {
+
     }
 
     @Test
