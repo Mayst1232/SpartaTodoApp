6a1c581aa8422bf8f4dddf37ed05bc04f52fd1d2
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-01 21:34:45 +0900

    refactory: Add @NoArgsConstructor

--- src/main/java/com/sparta/todoapp/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/todoapp/dto/SignupRequestDto.java
@@ -5,17 +5,17 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignupRequestDto {
-    @NotBlank
     @Pattern(regexp = "^[a-z0-9]*$")
     @Size(min = 4, max = 10)
     private String username;
 
-    @NotBlank
     @Pattern(regexp = "^[a-zA-Z0-9]*$")
     @Size(min = 8, max = 15)
     private String password;
