425bc0bf506f6167ab65f40dd183f64c6d1551de
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-01 12:06:34 +0900

    feature(Test): Add CardRepositoryTest
    1. init CardRepositoryTest
    
    2. extends RepositoryTest for use @DataJpaTest & @ActiveProfiles("test")

---
+++ src/test/java/com/sparta/todoapp/repository/CardRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.sparta.todoapp.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardRepositoryTest extends RepositoryTest {
+
+    @Autowired
+    CardRepository cardRepository;
+
+}
