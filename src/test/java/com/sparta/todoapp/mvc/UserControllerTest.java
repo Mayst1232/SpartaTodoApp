10bbe27b965f9a9318a153df2ffcc291867908b5
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-01 21:34:19 +0900

    feature : add signupTest in UserControllerTest

--- src/test/java/com/sparta/todoapp/mvc/UserControllerTest.java
+++ src/test/java/com/sparta/todoapp/mvc/UserControllerTest.java
@@ -1,10 +1,87 @@
 package com.sparta.todoapp.mvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.todoapp.config.WebSecurityConfig;
+import com.sparta.todoapp.controller.UserController;
+import com.sparta.todoapp.dto.SignupRequestDto;
+import com.sparta.todoapp.entity.User;
+import com.sparta.todoapp.security.UserDetailsImpl;
+import com.sparta.todoapp.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.security.Principal;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(
+        controllers = UserController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        classes = WebSecurityConfig.class
+                )
+        }
+)
+@ActiveProfiles("test")
 class UserControllerTest {
+    private MockMvc mvc;
+
+    private Principal mockPrincipal;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    UserService userService;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity(new MockSpringSecurityFilter()))
+                .build();
+    }
+
+    private void mockUserSetup() {
+        String username = "hwang1234";
+        String password = "qwer1234";
+        User testUser = new User(username, password);
+        UserDetailsImpl testUserDetails = new UserDetailsImpl(testUser);
+        mockPrincipal = new UsernamePasswordAuthenticationToken(testUserDetails,null, null);
+    }
 
     @Test
-    void signup() {
+    void signupSuccessTest() throws Exception {
+        SignupRequestDto requestDto = new SignupRequestDto("hwang1234","qwer1234");
+
+        String json = objectMapper.writeValueAsString(requestDto);
+
+        System.out.println(json);
+
+        mvc.perform(post("/api/user/signup")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
