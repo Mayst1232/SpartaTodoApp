6b40a9bee45b4288a297da9fb10ff5e2641a7d10
hwang <ghkdrbwjd369@naver.com>
2023-12-02 20:45:04 +0900

    refactory : Add @NoArgsConstructor in CardCompleteRequestDto

--- src/main/java/com/sparta/todoapp/dto/CardCompleteRequestDto.java
+++ src/main/java/com/sparta/todoapp/dto/CardCompleteRequestDto.java
@@ -2,10 +2,12 @@ package com.sparta.todoapp.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CardCompleteRequestDto {
     private boolean complete;
 }
