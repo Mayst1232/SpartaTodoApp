e951efd6a67bf856ff3036ea0df93280da2da424
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-04 01:14:53 +0900

    feature : Add Signup Success Test in UserServiceTest

--- src/test/java/com/sparta/todoapp/service/UserServiceTest.java
+++ src/test/java/com/sparta/todoapp/service/UserServiceTest.java
@@ -4,12 +4,15 @@ import com.sparta.todoapp.dto.SignupRequestDto;
 import com.sparta.todoapp.entity.User;
 import com.sparta.todoapp.repository.CardRepository;
 import com.sparta.todoapp.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -23,11 +26,12 @@ import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-    @InjectMocks
-    BCryptPasswordEncoder passwordEncoder;
 
     @InjectMocks
     UserService userService;
+
+    @Mock
+    PasswordEncoder passwordEncoder;
 
     @Mock
     UserRepository userRepository;
@@ -35,13 +39,15 @@ class UserServiceTest {
     @Test
     @DisplayName("회원가입 기능 성공 테스트")
     void signupSuccessTest() {
+        // given
         SignupRequestDto requestDto = new SignupRequestDto("hwang1234", "qwer1234");
-
-        String password = passwordEncoder.encode(requestDto.getPassword());
+        Mockito.when(passwordEncoder.encode(requestDto.getPassword())).thenReturn(new BCryptPasswordEncoder().encode(requestDto.getPassword()));
         given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.empty());
 
+        // when
         userService.signup(requestDto);
 
+        // then
         verify(userRepository, times(1)).save(any());
     }
 }
