9f13c6eb4302c45557098a59f8cde01939e23212
ghkdrbwjd369 <ghkdrbwjd369@naver.com>
2023-12-04 19:47:35 +0900

    feature : Add checkCommentTestSuccess in CommentServiceTest

--- src/test/java/com/sparta/todoapp/service/CommentServiceTest.java
+++ src/test/java/com/sparta/todoapp/service/CommentServiceTest.java
@@ -11,6 +11,8 @@ import com.sparta.todoapp.repository.CommentRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -38,6 +40,7 @@ class CommentServiceTest {
     @Test
     @DisplayName("댓글 작성 기능 성공 테스트")
     void createCommentTestSuccess() {
+        // given
         User user = new User();
         CardRequestDto requestDto = new CardRequestDto("카드 제목", "카드 내용", true, false);
         Card card = new Card(requestDto, user);
@@ -49,29 +52,36 @@ class CommentServiceTest {
 
         given(commentRepository.save(any())).willReturn(comment);
 
+        // when
         CommentResponseDto responseDto = commentService.createComments(card.getId(), commentRequestDto, user);
 
+        // then
         assertThat("댓글 내용").isEqualTo(responseDto.getContent());
     }
 
     @Test
     @DisplayName("댓글 작성 기능 실패 테스트")
     void createCommentTestFailCardIsNotExist() {
+        // given
         User user = new User();
         Card card = new Card();
         CommentRequestDto commentRequestDto = new CommentRequestDto("댓글 내용");
 
         given(cardRepository.findById(card.getId())).willReturn(Optional.empty());
 
+        // when
         Exception exception = assertThrows(NullPointerException.class,
                 () -> commentService.createComments(card.getId(), commentRequestDto, user)
         );
 
+        // then
         assertThat("해당 카드는 존재하지 않습니다.").isEqualTo(exception.getMessage());
     }
 
     @Test
+    @DisplayName("선택한 카드 안에 있는 전체 댓글 조회 기능 테스트")
     void getCommentsTest() {
+        // given
         Card card = new Card();
         User user = new User();
         user.setUsername("hwang");
@@ -86,11 +96,39 @@ class CommentServiceTest {
 
         given(commentRepository.findAllByCardId(any())).willReturn(commentList);
 
+        // when
         List<CommentResponseDto> commentResponseDtoList = commentService.getComments(card.getId());
 
+        // then
         assertThat(commentResponseDtoList).hasSize(5);
         assertThat(commentResponseDtoList.get(0).getContent()).isEqualTo("댓글 내용 0");
     }
 
+    @Test
+    @DisplayName("유저의 코멘트만 수정 / 삭제 할 수 있기 때문에 자신의 코멘트를 찾는 기능 성공 테스트")
+    void checkCommentTestSuccess() {
+        Comment myComment;
+        List<Comment> commentList = new ArrayList<>();
+        Card card = new Card();
+        card.setId(1L);
+        User user = new User();
+        user.setUsername("hwang");
 
+        for(int i = 0; i < 5; i++){
+            CommentRequestDto requestDto = new CommentRequestDto("댓글 내용 " + i);
+
+            Comment comment = new Comment(requestDto.getContent(), user.getUsername(), user, card);
+            comment.setId((long) i+1);
+            commentList.add(comment);
+        }
+
+        Comment checkComment = commentList.get(0);
+
+        given(commentRepository.findAllByUser(user)).willReturn(commentList);
+        given(commentRepository.findById(checkComment.getId())).willReturn(Optional.of(checkComment));
+
+        myComment = commentService.checkComment(checkComment.getId(), user);
+
+        assertThat("댓글 내용 0").isEqualTo(myComment.getContent());
+    }
 }
